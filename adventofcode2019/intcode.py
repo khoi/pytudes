c3a4e7ea11954bdb5f29459fbbc1175dc171ad15
khoi <khoiracle@gmail.com>
2019-12-17 14:52:05 +0700

    Refactor intcode

--- adventofcode2019/intcode.py
+++ adventofcode2019/intcode.py
@@ -78,63 +78,84 @@ class IntCode:
         return outputs
 
 
-# Some assertions to make sure Intcode works properly
-# https://adventofcode.com/2019/day/5
-test_prog = [
-    3,
-    21,
-    1008,
-    21,
-    8,
-    20,
-    1005,
-    20,
-    22,
-    107,
-    8,
-    21,
-    20,
-    1006,
-    20,
-    31,
-    1106,
-    0,
-    36,
-    98,
-    0,
-    0,
-    1002,
-    21,
-    125,
-    20,
-    4,
-    20,
-    1105,
-    1,
-    46,
-    104,
-    999,
-    1105,
-    1,
-    46,
-    1101,
-    1000,
-    1,
-    20,
-    4,
-    20,
-    1105,
-    1,
-    46,
-    98,
-    99,
-]
-assert IntCode(test_prog).run([7])[-1] == 999
-assert IntCode(test_prog).run([8])[-1] == 1000
-assert IntCode(test_prog).run([9])[-1] == 1001
+if __name__ == "__main__":
+    # Some assertions to make sure Intcode works properly
+    # https://adventofcode.com/2019/day/5
+    test_prog = [
+        3,
+        21,
+        1008,
+        21,
+        8,
+        20,
+        1005,
+        20,
+        22,
+        107,
+        8,
+        21,
+        20,
+        1006,
+        20,
+        31,
+        1106,
+        0,
+        36,
+        98,
+        0,
+        0,
+        1002,
+        21,
+        125,
+        20,
+        4,
+        20,
+        1105,
+        1,
+        46,
+        104,
+        999,
+        1105,
+        1,
+        46,
+        1101,
+        1000,
+        1,
+        20,
+        4,
+        20,
+        1105,
+        1,
+        46,
+        98,
+        99,
+    ]
+    assert IntCode(test_prog).run([7])[-1] == 999
+    assert IntCode(test_prog).run([8])[-1] == 1000
+    assert IntCode(test_prog).run([9])[-1] == 1001
 
-quine_prog = [109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99]
-assert IntCode(quine_prog).run([]) == quine_prog
+    quine_prog = [
+        109,
+        1,
+        204,
+        -1,
+        1001,
+        100,
+        1,
+        100,
+        1008,
+        100,
+        16,
+        101,
+        1006,
+        101,
+        0,
+        99,
+    ]
+    assert IntCode(quine_prog).run([]) == quine_prog
 
-assert IntCode([1102, 34915192, 34915192, 7, 4, 7, 99, 0]).run([]) == [1219070632396864]
-assert IntCode([104, 1125899906842624, 99]).run([]) == [1125899906842624]
+    assert IntCode([1102, 34915192, 34915192, 7, 4, 7, 99, 0]).run([]) == [
+        1219070632396864
+    ]
+    assert IntCode([104, 1125899906842624, 99]).run([]) == [1125899906842624]
+    print("IntCode seems to be ok")
