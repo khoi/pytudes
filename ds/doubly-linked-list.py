c82ce3497859b93d239fd06dc810a29b4996559c
khoi <khoiracle@gmail.com>
2020-01-25 17:46:22 +0700

    Doubly linked list

--- ds/doubly-linked-list.py
+++ ds/doubly-linked-list.py
@@ -4,6 +4,9 @@ class Node:
         self.next = None
         self.previous = None
 
+    def __str__(self):
+        return str(self.value)
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -12,17 +15,61 @@ class LinkedList:
     def is_empty(self):
         return not self.head
 
+    @property
+    def tail(self):
+        current = self.head
+        while current and current.next:
+            current = current.next
+        return current
+
     def __len__(self):
         count = 0
         current = self.head
         while current:
             count += 1
-            current = self.head.next
+            current = current.next
         return count
+
+    def append(self, value):
+        newNode = Node(value)
+        tail = self.tail
+        if tail:
+            newNode.previous = tail
+            tail.next = newNode
+        else:
+            self.head = newNode
+
+    def prepend(self, value):
+        newNode = Node(value)
+        if self.head:
+            self.head.previous = newNode
+            newNode.next = self.head
+        self.head = newNode
+
 
 if __name__ == "__main__":
     ll = LinkedList()
 
     assert ll.is_empty == True
     assert len(ll) == 0
+    assert ll.head is None
+    assert ll.tail is None
+
+    ll.append(1)
+    assert ll.is_empty == False
+    assert len(ll) == 1
+    assert ll.head.value is 1
+    assert ll.tail.value is 1
+
+    for v in [2,3,4,5]:
+        ll.append(v)
+
+    assert len(ll) == 5
+    assert ll.head.value == 1
+    assert ll.tail.value == 5
+
+    ll.prepend(0)
+    assert len(ll) == 6
+    assert ll.head.value == 0
+    assert ll.tail.value == 5
     print("Doubly test passed")
