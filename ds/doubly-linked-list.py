e6c45f7567c28ced09f6d381f81aa9b20b40dcfc
khoi <khoiracle@gmail.com>
2020-01-26 21:37:54 +0700

    Finish doubly linkedlist implementation and tests

--- ds/doubly-linked-list.py
+++ ds/doubly-linked-list.py
@@ -7,6 +7,7 @@ class Node:
     def __str__(self):
         return str(self.value)
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -33,6 +34,19 @@ class LinkedList:
             current = current.next
         return count
 
+    def __node(self, index):
+        current = self.head
+        count = 0
+        while current:
+            if count == index:
+                return current
+            current = current.next
+            count += 1
+        raise Exception("index out of bounds")
+    
+    def __getitem__(self, key):
+        return self.__node(key).value
+
     def append(self, value):
         newNode = Node(value)
         tail = self.tail
@@ -49,6 +63,22 @@ class LinkedList:
             newNode.next = self.head
         self.head = newNode
 
+    def __remove(self, node):
+        previous = node.previous
+        next = node.next 
+        if previous:
+            previous.next = next
+        else:
+            self.head = next
+        if next:
+            next.previous = previous
+        return node
+
+    def remove(self, index):
+        node = self.__node(index)
+        return self.__remove(node).value
+
+
 if __name__ == "__main__":
     ll = LinkedList()
 
@@ -56,24 +86,35 @@ if __name__ == "__main__":
     assert len(ll) == 0
     assert ll.head is None
     assert ll.tail is None
-    assert not ll # test __bool__
+    assert not ll  # test __bool__
 
-    ll.append(1)
+    ll.append(0)
     assert ll.is_empty == False
     assert len(ll) == 1
-    assert ll.head.value == 1
-    assert ll.tail.value == 1
-    assert ll # test __bool__
+    assert ll.head.value == 0
+    assert ll.tail.value == 0
+    assert ll  # test __bool__
+    assert ll[0] == 0
 
-    for v in [2,3,4,5]:
+    for v in [1, 2, 3, 4, 5]:
         ll.append(v)
 
-    assert len(ll) == 5
-    assert ll.head.value == 1
-    assert ll.tail.value == 5
-
-    ll.prepend(0)
     assert len(ll) == 6
     assert ll.head.value == 0
     assert ll.tail.value == 5
+    for i in range(0, 6):
+        assert ll[i] == i
+
+    ll.prepend(-1)
+    assert len(ll) == 7
+    assert ll.head.value == -1 
+    assert ll.tail.value == 5
     print("Doubly test passed")
+
+    assert ll.remove(0) == -1 # remaining: [0, 1, 2, 3, 4, 5]
+    assert ll.remove(0) == 0 # remaining: [1, 2, 3, 4, 5]
+    assert ll.remove(4) == 5 # remaining: [1, 2, 3, 4]
+    for i in range(0, len(ll)):
+        assert ll.remove(0) == i + 1
+    assert len(ll) == 0
+    assert ll.is_empty 
