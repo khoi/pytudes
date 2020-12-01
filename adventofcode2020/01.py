070c53ce4b2ac0f585be853d2fe14179fd00cccf
khoi <git@khoi.io>
2020-12-01 22:44:23 +0700

    clenaup

--- adventofcode2020/01.py
+++ adventofcode2020/01.py
@@ -7,8 +7,7 @@ def prod_of_two_pairs_that_sum_to_a_target(s, target):
 
 def prod_of_3_pairs_that_sum_to_a_target(s, target):
     for v in s:
-        d = target - v
-        prod_of_two_pairs = prod_of_two_pairs_that_sum_to_a_target(s, d)
+        prod_of_two_pairs = prod_of_two_pairs_that_sum_to_a_target(s - {v}, target - v)
         if prod_of_two_pairs:
             return v * prod_of_two_pairs
     return None
@@ -16,6 +15,7 @@ def prod_of_3_pairs_that_sum_to_a_target(s, target):
 
 f = open("inputs/01.txt")
 numbers = set([int(v) for v in f.read().splitlines()])
+
 
 print(prod_of_two_pairs_that_sum_to_a_target(numbers, 2020))
 print(prod_of_3_pairs_that_sum_to_a_target(numbers, 2020))
