a43a426743e2eb87cbe92abac504b91c14fb857d
khoi <git@khoi.io>
2020-12-01 23:04:05 +0700

    englishh

--- adventofcode2020/01.py
+++ adventofcode2020/01.py
@@ -1,21 +1,19 @@
-def prod_of_two_pairs_that_sum_to_a_target(s, target):
+def prod_of_two_that_sum_to_target(s, target):
     for v in s:
         d = target - v
         if d in s:
             return v * d
     return None
 
-def prod_of_3_pairs_that_sum_to_a_target(s, target):
+def prod_of_3_that_sum_to_a_target(s, target):
     for v in s:
-        prod_of_two_pairs = prod_of_two_pairs_that_sum_to_a_target(s - {v}, target - v)
-        if prod_of_two_pairs:
-            return v * prod_of_two_pairs
+        prod_of_two = prod_of_two_that_sum_to_target(s - {v}, target - v)
+        if prod_of_two:
+            return v * prod_of_two
     return None
-        
 
 f = open("inputs/01.txt")
 numbers = set([int(v) for v in f.read().splitlines()])
 
-
-print(prod_of_two_pairs_that_sum_to_a_target(numbers, 2020))
-print(prod_of_3_pairs_that_sum_to_a_target(numbers, 2020))
+print(prod_of_two_that_sum_to_target(numbers, 2020))
+print(prod_of_3_that_sum_to_a_target(numbers, 2020))
