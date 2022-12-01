6f60130dace58a78b8e73d154eb703fc5366e4c6
khoi <git@khoi.io>
2022-12-01 12:21:17 +0700

    Day1

--- aoc2022/src/bin/01.rs
+++ aoc2022/src/bin/01.rs
@@ -1,11 +1,28 @@
 use aoc2022::read_file_input;
+use std::collections::BinaryHeap;
 
 fn main() {
     let input = read_file_input(01);
 
-    for (i, line) in input.lines().enumerate() {
-        println!("Line {} contains {}", i, line);
-    }
+    let mut heap: BinaryHeap<i32> = BinaryHeap::new();
 
-    println!("Hello from Day 1");
+    let mut curr_iter_max = 0;
+    input.lines().for_each(|line| {
+        if line.is_empty() {
+            heap.push(curr_iter_max);
+            curr_iter_max = 0;
+            return;
+        }
+
+        let num = line.parse::<i32>().unwrap();
+        curr_iter_max += num;
+    });
+
+    let top3 = [
+        heap.pop().unwrap(),
+        heap.pop().unwrap(),
+        heap.pop().unwrap(),
+    ];
+    println!("{}", top3[0]);
+    println!("{}", top3.iter().sum::<i32>());
 }
