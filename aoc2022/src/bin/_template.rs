247deb936619066fef08e3b216744d0e6217026a
khoi <git@khoi.io>
2022-12-08 21:07:09 +0700

    Add _template

---
+++ aoc2022/src/bin/_template.rs
@@ -0,0 +1,42 @@
+use aoc2022::read_file_input;
+
+type Input<'a> = Vec<&'a str>;
+
+fn parse(input: &str) -> Input {
+    input.lines().collect()
+}
+
+pub fn part1(input: &Input) -> u64 {
+    1
+}
+
+fn part2(input: &Input) -> u64 {
+    2
+}
+
+fn main() {
+    let input = read_file_input(04);
+    let parsed = parse(&input);
+
+    println!("{}", part1(&parsed));
+    println!("{}", part2(&parsed));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static INPUT: &str = "";
+
+    #[test]
+    fn test_1() {
+        let result = part1(&parse(INPUT));
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn test_2() {
+        let result = part2(&parse(INPUT));
+        assert_eq!(result, 4);
+    }
+}
