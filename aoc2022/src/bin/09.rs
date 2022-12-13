44c9e6dffad9bba72edf0016d0ed13df62baf391
khoi <git@khoi.io>
2022-12-13 13:09:45 +0700

    Refactor 09 to use InfiniteGrid

--- aoc2022/src/bin/09.rs
+++ aoc2022/src/bin/09.rs
@@ -1,81 +1,53 @@
 use std::collections::{HashSet};
-use aoc2022::Point;
+use aoc2022::{Direction, Point, InfiniteGrid};
+use std::cmp;
 
 use aoc2022::read_file_input;
 
-type Input<'a> = Vec<(char, usize)>;
+type Input<'a> = Vec<(Direction, usize)>;
 
 fn parse(input: &str) -> Input {
-    input.lines().map(|l| {
-        let w = l.split_whitespace().collect::<Vec<_>>();
-        (w[0].chars().next().unwrap(), w[1].parse::<usize>().unwrap())
+    input.lines().filter_map(|l| {
+        let (dir, dist) = l.split_once(' ')?;
+        let dist = dist.parse().ok()?;
+            let step = match dir {
+                "R" => (Direction::E, dist),
+                "U" => (Direction::N, dist),
+                "L" => (Direction::W, dist),
+                "D" => (Direction::S, dist),
+                _ => unreachable!(),
+            };
+            Some(step)
     }).collect()
 }
 
-fn is_touching(head: &Point, tail: &Point) -> bool {
-    let del_x = (head.x - tail.x).abs();
-    let del_y = (head.y - tail.y).abs();
-    match (del_x, del_y) {
-        (0, 0) => true,
-        (1, 1) => true,
-        (1, 0) => true,
-        (0, 1) => true,
-        _ => false,
-    }
-}
-
-fn follow_instruction_directions(head: &Point, tail: &Point) -> Vec<char> {
-    if is_touching(&head, &tail) {
-        return vec![];
-    }
-
-    let mut directions = vec![];
-    let del_x = head.x - tail.x;
-    let del_y = head.y - tail.y;
-    
-    if del_x != 0 {
-        directions.push(if del_x > 0 {'R'} else {'L'});
-    }
-
-    if del_y != 0 {
-        directions.push(if del_y > 0 {'U'} else {'D'});
-    }
-
-    directions
-}
-
-fn new_pos(pos: &Point, dir: char, dist: usize) -> Point {
-    match dir {
-        'R' => Point {x: pos.x + dist as isize,y: pos.y},
-        'L' => Point {x: pos.x - dist as isize,y: pos.y},
-        'U' => Point {x: pos.x,y: pos.y + dist as isize},
-        'D' => Point {x: pos.x,y: pos.y - dist as isize},
-        _ => panic!("Invalid direction"),
+fn calculate_new_tail_position(head: &Point, tail: &Point) -> Point {
+    Point {
+        x: tail.x + cmp::max(cmp::min(head.x - tail.x, 1), -1),
+        y: tail.y + cmp::max(cmp::min(head.y - tail.y, 1), -1),
     }
 }
 
 fn count_tail_moves(input: &Input) -> usize {
     let mut current_head_pos= Point { x: 0, y: 0 };
     let mut current_tail_pos= Point { x: 0, y: 0 };
-    let mut visited: HashSet<Point> = HashSet::new();
-    visited.insert(current_tail_pos.clone());
+    
+    let mut tail_grid = InfiniteGrid::new();
+    tail_grid.insert(current_tail_pos.clone(), 1);
 
     input.iter().for_each(|f| {
         for _ in 0..f.1 {
-            current_head_pos = new_pos(&current_head_pos, f.0, 1);
-            let follow_instructions = follow_instruction_directions(&current_head_pos, &current_tail_pos);
-
-            for i in 0..follow_instructions.len() {
-                let is_diagonal = follow_instructions.len() == 2;
-                current_tail_pos = new_pos(&current_tail_pos, follow_instructions[i], 1);
-                if is_diagonal && i == 0 { // skip the first step of a diagonal
-                    continue;
-                }
-                visited.insert(current_tail_pos.clone());
+            current_head_pos = current_head_pos.get_neighbor(&f.0);
+            let new_tail_pos = calculate_new_tail_position(&current_head_pos, &current_tail_pos);
+            
+            let tails_move = current_head_pos.chebyshev_distance(&current_tail_pos) > 1; 
+            if tails_move {
+                current_tail_pos = new_tail_pos;
+                tail_grid.insert(current_tail_pos.clone(), 1);
             }
         }
     });
-    visited.len() 
+    tail_grid.points.len() 
 }
 
 fn part1(input: Input) -> usize {
@@ -113,9 +85,9 @@ R 2";
         assert_eq!(result, 13);
     }
 
-    #[test]
-    fn test_2() {
-        let result = part2(parse(INPUT));
-        assert_eq!(result, 4);
-    }
+    // #[test]
+    // fn test_2() {
+    //     let result = part2(parse(INPUT));
+    //     assert_eq!(result, 4);
+    // }
 }
--- aoc2022/src/lib.rs
+++ aoc2022/src/lib.rs
@@ -1,4 +1,5 @@
 use std::{env, fs};
+use std::collections::{HashMap};
 
 pub fn read_file_input(day: u8) -> String {
     let cwd = env::current_dir().unwrap();
@@ -7,8 +8,65 @@ pub fn read_file_input(day: u8) -> String {
     fs::read_to_string(filepath).unwrap()
 }
 
+#[derive(Clone)]
+pub enum Direction {
+    N,
+    NE,
+    E,
+    SE,
+    S,
+    SW,
+    W,
+    NW,
+}
+
+
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub struct Point {
     pub x: isize,
     pub y: isize,
+}
+
+impl Point {
+    pub fn manhattan_distance(&self, other: &Point) -> usize {
+        ((self.x - other.x).abs() + (self.y - other.y).abs()) as usize
+    }
+
+    pub fn chebyshev_distance(&self, other: &Point) -> usize {
+        ((self.x - other.x).abs().max((self.y - other.y).abs())) as usize
+    }
+
+    pub fn get_neighbor(&self, direction: &Direction) -> Point {
+        match direction {
+            Direction::N => Point { x: self.x, y: self.y + 1 },
+            Direction::NE => Point { x: self.x + 1, y: self.y + 1 },
+            Direction::E => Point { x: self.x + 1, y: self.y },
+            Direction::SE => Point { x: self.x + 1, y: self.y - 1 },
+            Direction::S => Point { x: self.x, y: self.y - 1 },
+            Direction::SW => Point { x: self.x - 1, y: self.y - 1 },
+            Direction::W => Point { x: self.x - 1, y: self.y },
+            Direction::NW => Point { x: self.x - 1, y: self.y + 1 },
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct InfiniteGrid<T> {
+    pub points: HashMap<Point, T>,
+}
+
+impl<T> InfiniteGrid<T> {
+    pub fn new() -> InfiniteGrid<T> {
+        InfiniteGrid {
+            points: HashMap::new(),
+        }
+    }
+
+    pub fn get(&self, point: &Point) -> Option<&T> {
+        self.points.get(point)
+    }
+
+    pub fn insert(&mut self, point: Point, data: T) {
+        self.points.insert(point, data);
+    }
 }
