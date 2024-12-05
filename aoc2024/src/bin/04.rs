1e15cf9e12c62ebdaeb94f453225fe3812b82edc
khoi <git@khoi.io>
2024-12-05 05:42:05 +0000

    Day 4 start

---
+++ aoc2024/src/bin/04.rs
@@ -0,0 +1,42 @@
+use aoc2024::read_file_input;
+
+type Input<'a> = Vec<&'a str>;
+
+fn parse(input: &str) -> Input {
+    input.lines().collect()
+}
+
+fn part1(input: Input) -> usize {
+    1
+}
+
+fn part2(input: Input) -> usize {
+    2
+}
+
+fn main() {
+    let input = read_file_input(1);
+    let parsed = parse(&input);
+
+    println!("{}", part1(parsed.clone()));
+    println!("{}", part2(parsed.clone()));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static INPUT: &str = "";
+
+    #[test]
+    fn test_1() {
+        let result = part1(parse(INPUT));
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn test_2() {
+        let result = part2(parse(INPUT));
+        assert_eq!(result, 2);
+    }
+}
