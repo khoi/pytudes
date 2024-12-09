a58db80880e61b4e24f845fe864dd90a82bbc2d9
khoi <git@khoi.io>
2024-12-09 16:34:11 +0000

    Day 9 again

--- aoc2024/src/bin/09.rs
+++ aoc2024/src/bin/09.rs
@@ -2,55 +2,52 @@ use aoc2024::read_file_input;
 
 type Input<'a> = &'a str;
 
-fn expand_dense_format(input: &str) -> String {
-    input
-        .chars()
-        .collect::<Vec<_>>()
-        .chunks(2)
-        .enumerate()
-        .fold(String::new(), |mut acc, (i, chunk)| {
-            acc.push_str(
-                &i.to_string()
-                    .repeat(chunk[0].to_digit(10).unwrap() as usize),
-            );
-            if chunk.len() == 2 {
-                acc.push_str(&".".repeat(chunk[1].to_digit(10).unwrap() as usize));
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct File {
+    id: usize,
+    length: usize,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum Entry {
+    Free(usize),
+    File(File),
+}
+
+struct FS {
+    entries: Vec<Entry>,
+}
+
+impl std::fmt::Display for FS {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for e in self.entries.iter() {
+            match e {
+                Entry::Free(size) => write!(f, "{}", ".".repeat(*size))?,
+                Entry::File(file) => write!(f, "{}", file.id.to_string().repeat(file.length))?,
             }
-            acc
-        })
-}
-
-fn compact(input: &str) -> String {
-    let mut chars: Vec<char> = input.chars().collect();
-    let mut left = 0;
-    let mut right = chars.len() - 1;
-
-    while left < right {
-        // Move left pointer until we find a dot
-        while left < right && chars[left] != '.' {
-            left += 1;
         }
 
-        // Move right pointer until we find a digit
-        while left < right && !chars[right].is_ascii_digit() {
-            right -= 1;
-        }
-
-        // If pointers are still valid, swap the characters
-        if left < right {
-            chars.swap(left, right);
-            left += 1;
-            right -= 1;
-        }
+        Ok(())
     }
-
-    chars.into_iter().collect()
 }
 
-fn checksum(input: &str) -> u64 {
-    input.chars().enumerate().fold(0, |acc, (i, c)| {
-        acc + (i as u64) * c.to_digit(10).unwrap_or(0) as u64
-    })
+impl std::str::FromStr for FS {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut entries = vec![];
+        for (i, c) in s.chars().enumerate() {
+            if i % 2 == 0 {
+                entries.push(Entry::File(File {
+                    id: i / 2,
+                    length: c.to_digit(10).unwrap() as usize,
+                }));
+            } else {
+                entries.push(Entry::Free(c.to_digit(10).unwrap() as usize));
+            }
+        }
+        Ok(FS { entries })
+    }
 }
 
 fn parse(input: &str) -> Input {
@@ -58,13 +55,13 @@ fn parse(input: &str) -> Input {
 }
 
 fn part1(input: Input) -> u64 {
-    let expanded = expand_dense_format(input);
-    let compacted = compact(&expanded);
-    checksum(&compacted)
+    let fs = input.parse::<FS>().unwrap();
+    println!("{}", fs);
+    1
 }
 
 fn part2(input: Input) -> usize {
-    input.len()
+    2
 }
 
 fn main() {
@@ -82,26 +79,9 @@ mod tests {
     static INPUT: &str = "2333133121414131402";
 
     #[test]
-    fn test_expand_dense() {
-        let result = expand_dense_format(INPUT);
-        assert_eq!(result, "00...111...2...333.44.5555.6666.777.888899");
-    }
-
-    #[test]
-    fn test_checksum() {
-        let result = checksum("0099811188827773336446555566..............");
-        assert_eq!(result, 1928);
-    }
-
-    #[test]
-    fn test_compact() {
-        let result = compact("00...111...2...333.44.5555.6666.777.888899");
-        assert_eq!(result, "0099811188827773336446555566..............");
-    }
-    #[test]
-    fn test_1() {
-        let result = part1(parse(INPUT));
-        assert_eq!(result, 1928);
+    fn test_fs() {
+        let fs = INPUT.parse::<FS>().unwrap();
+        assert_eq!(fs.to_string(), "00...111...2...333.44.5555.6666.777.888899");
     }
 
     // #[test]
