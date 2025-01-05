a93bb2be216fe9f73bf93ec62481b6018c9ff8d2
khoi <git@khoi.io>
2025-01-05 21:51:34 +0700

    23 part 1

--- aoc2024/src/bin/23.rs
+++ aoc2024/src/bin/23.rs
@@ -1,44 +1,122 @@
 #![allow(warnings)]
 
 use aoc2024::read_file_input;
+use std::collections::{HashMap, HashSet};
 
-type Input<'a> = &'a str;
+type Graph = HashMap<String, HashSet<String>>;
 
-fn parse(input: &str) -> Input {
-    input.trim()
+fn parse(input: &str) -> Graph {
+    let mut graph: Graph = HashMap::new();
+
+    for line in input.trim().lines() {
+        let (a, b) = line.split_once('-').unwrap();
+        graph
+            .entry(a.to_string())
+            .or_default()
+            .insert(b.to_string());
+        graph
+            .entry(b.to_string())
+            .or_default()
+            .insert(a.to_string());
+    }
+
+    graph
 }
 
-fn part1(input: Input) -> usize {
-    input.len()
+fn find_triangles(graph: &Graph) -> Vec<[String; 3]> {
+    let mut triangles = Vec::new();
+    let nodes: Vec<_> = graph.keys().cloned().collect();
+
+    for (i, a) in nodes.iter().enumerate() {
+        if let Some(a_neighbors) = graph.get(a) {
+            for b in a_neighbors {
+                if b <= a {
+                    continue;
+                }
+                if let Some(b_neighbors) = graph.get(b) {
+                    for c in b_neighbors {
+                        if c <= b {
+                            continue;
+                        }
+                        if a_neighbors.contains(c) {
+                            triangles.push([a.clone(), b.clone(), c.clone()]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    triangles
 }
 
-fn part2(input: Input) -> usize {
-    input.len()
+fn part1(graph: Graph) -> usize {
+    let triangles = find_triangles(&graph);
+    triangles
+        .iter()
+        .filter(|[a, b, c]| a.starts_with('t') || b.starts_with('t') || c.starts_with('t'))
+        .count()
+}
+
+fn part2(graph: Graph) -> usize {
+    0 // Not implemented yet
 }
 
 fn main() {
-    let input = read_file_input(1);
+    let input = read_file_input(23);
     let parsed = parse(&input);
 
-    println!("{}", part1(parsed));
-    println!("{}", part2(parsed));
+    println!("{}", part1(parsed.clone()));
+    println!("{}", part2(parsed.clone()));
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    static INPUT: &str = "";
+    static INPUT: &str = "\
+kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn";
 
     #[test]
     fn test_1() {
         let result = part1(parse(INPUT));
-        assert_eq!(result, 1);
+        assert_eq!(result, 7);
     }
 
-    #[test]
-    fn test_2() {
-        let result = part2(parse(INPUT));
-        assert_eq!(result, 2);
-    }
+    // #[test]
+    // fn test_2() {
+    //     let result = part2(parse(INPUT));
+    //     assert_eq!(result, 0);
+    // }
 }
